7617faff482bc3fa4172a925046b9eb5702f3e52
Aleks Ivanov <zuodion@gmail.com>
2020-03-03 12:03:50 +0200

    git init

---
+++ server.js
@@ -0,0 +1,36 @@
+var soap = require('soap');
+var express = require('express');
+var app = express();
+
+var service = {
+    calculator_service: {
+        caclulator_port: {
+            calculate: (args) => {
+                return {response: `Answer of ${args.a} + ${args.b} is ${+args.a + +args.b}`};
+            }
+        }
+    }
+}
+// xml data is extracted from wsdl file created
+var xml = require('fs').readFileSync('./calculator.wsdl', 'utf8');
+
+var server = app.listen(3030);
+soap.listen(server, '/calculator', service, xml);
+
+
+// Client side
+var url = "http://localhost:3030/calculator?wsdl";
+var args = { a: 5, b: 10 };
+soap.createClient(url, function (err, client) {
+    if (err)
+        console.error(err);
+    else {
+        client.calculate(args, function (err, response) {
+            if (err)
+                console.error(err);
+            else {
+                console.log(response);
+            }
+        })
+    }
+});
